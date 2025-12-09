fbd7a4625ca6d6d828274f7a2699415ffd6bf509
Närin <narin@hotmail.fi>
2025-12-09 18:41:42 +0200

    Auto-luokan luonti
    Loin Auto-luokan, jonne määrittelin attribuutteja ja metodin.

---
+++ App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+} // Appin loppu
+
+//Luodaan Auto-luokka
+class Auto {
+
+   //Attributes
+    String merkki; 
+    String malli;
+    int bensanMaara;
+
+    //Metodit
+        public void jarruta() {
+            System.out.println("Auto jarruttaa.");
+        }
+
+}
