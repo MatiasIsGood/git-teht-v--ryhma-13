02f2d2c53f5a274bd62efaa462beb54a3c7dee78
MatiasIsGood <76411418+MatiasIsGood@users.noreply.github.com>
2025-12-16 23:00:08 +0200

    Antaa arvot autolle ja printtaa ne

--- App.java
+++ App.java
@@ -1,6 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Auto auto1 = new Auto();
+
+        //antaa arvo autolle
+        auto1.merkki = "VW";
+        auto1.malli = "Golf";
+        auto1.bensanMaara = 50;
+
+        //tulostaa tiedot jos bensa on yli 0, muuten jarruttaa
+        if(auto1.bensanMaara > 0) {
+            auto1.tulostaTiedot();
+        } else {
+            auto1.jarruta();
+        }
     }
 } // Appin loppu
 
@@ -21,6 +33,4 @@ class Auto {
         System.out.println("Malli: " + malli);
         System.out.println("Bensan määrä: " + bensanMaara);
     }
-
 }
-
